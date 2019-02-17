3e98444899f5a82f0d847a7a4d4baf453b8207b8
Peter Collingridge <peter.collingridge@gmail.com>
2019-02-17 23:30:08 +0000

    Get graph working with sigmoid function

--- neural-net.js
+++ neural-net.js
@@ -2,6 +2,7 @@
 Vue.component('neuron-graph', {
     template: '#neuron-graph-el',
     props: [
+        'function',
         'input',
         'weight',
         'bias',
@@ -10,23 +11,25 @@ Vue.component('neuron-graph', {
     data: function() {
         return {
             width: 200,
+            height: 100,
             minBias: -5,
             maxBias: 5,
             biasModel: this.bias || 0
         };
     },
     computed: {
-        stepFunction: function() {
-            return "M0 100H" + this.scaleX(this.biasModel) + "V0 H" + this.width;
-        },
-        sigmoidPath: function() {
-            var path = "M";
-            var bias = +this.biasModel;
-
-            for (var x = -10; x <= 10; x += 0.25) {
-                path += this.scaleX(x) + " " + (100 * (1 - sigmoid(x + bias))) + " ";
+        path: function() {
+            if (this.function === 'step') {
+                return "M0 100H" + this.scaleX(this.biasModel) + "V0 H" + this.width;
+            } else {
+                var path = "M";
+                var bias = +this.biasModel;
+    
+                for (var x = -10; x <= 10; x += 0.25) {
+                    path += this.scaleX(x) + " " + this.scaleY(this.sigmoid(x - bias)) + " ";
+                }
+                return path;
             }
-            return path;
         },
         inputX: function() {
             return this.scaleX(this.input * this.weight);
@@ -38,12 +41,25 @@ Vue.component('neuron-graph', {
             return (this.width - this.domainWidth) * 0.5;
         },
         output: function() {
-            return this.input * this.weight > this.biasModel;
+            if (this.function === 'step') {
+                return this.input * this.weight > this.biasModel ? 1 : 0;
+            } else {
+                return this.sigmoid(this.input * this.weight - this.biasModel);
+            }
         }
     },
     methods: {
         scaleX: function(x) {
             return this.width * 0.5 + (x * this.width * 0.05);
+        },
+        scaleY: function(y) {
+            return this.height * (1 - y);
+        },
+        sigmoid: function(x) {
+            return 1 / (1 + Math.exp(-x));
+        },
+        round: function(x) {
+            return Math.round(x * 1000) / 1000;
         }
     },
 });
@@ -68,8 +84,3 @@ var vm = new Vue({
         }
     },
 });
-
-
-function sigmoid(x) {
-    return 1 / (1 + Math.exp(-x));
-}
