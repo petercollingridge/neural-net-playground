1053d4afef10e5277b1418708217b8ace9cc30e7
Peter Collingridge <peter.collingridge@gmail.com>
2019-02-17 15:38:09 +0000

    Show input domain

--- neural-net.js
+++ neural-net.js
@@ -2,6 +2,10 @@ var vm = new Vue({
     el: '#neural-net',
     data: {
         width: 200,
+        minWeight: -5,
+        maxWeight: 5,
+        minBias: -5,
+        maxBias: 5,
         'input1': 0.2,
         'weight1a': 1,
         'weight1b': 0,
@@ -21,16 +25,21 @@ var vm = new Vue({
         max1: function() {
             return this.weight1a - this.bias1;
         },
-        'bias1width': function() {
+        bias1width: function() {
             return Math.abs(this.weight1a) * this.width / 10;
         },
-        'bias1x': function() {
+        bias1x: function() {
             var x = this.min1 < this.max1 ? this.min1 : this.max1;
-            x = Math.max(x, -10);
-            return (x * this.width * 0.05) + this.width * 0.5;
+            return this.scaleX(x);
+        },
+        point1x: function() {
+            return this.scaleX(this.input1 * this.weight1a - this.bias1);
         }
     },
     methods: {
+        scaleX: function(x) {
+            return this.width * 0.5 + (x * this.width * 0.05);
+        }
     },
     watch: {
     }
